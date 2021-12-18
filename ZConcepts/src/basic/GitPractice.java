4b670f99c74a521a02282a8a5be1167ede92cac4
L340 <L340@LAPTOP-D6FDH20G>
2021-12-18 17:18:41 +0530

    yessss

---
+++ ZConcepts/src/basic/GitPractice.java
@@ -0,0 +1,21 @@
+package basic;
+
+public class GitPractice {
+
+	public static void main(String[] args) {
+		
+		System.out.println("colloboration successful");
+		System.out.println("Enter a number");
+		int i = 50;
+		if(i%2==0)
+		{
+			System.out.println("even");
+		}else
+		{
+			System.out.println("odd");
+		}
+			
+
+	}
+
+}
---
+++ ZConcepts/src/basic/Loops.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Loopsghhhhhhhhhkkkkkkkkkkkkkkkkkhh");
+        System.out.println("Modified after 1st sprintsssssssssssss");
+	}
+
+}
